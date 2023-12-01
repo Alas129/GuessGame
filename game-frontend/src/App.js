3125167e26bee6b9a44d642bc0b732e9abfb0661
Alas129 <alas129@163.com>
2023-12-01 12:18:44 -0800

    v1: updated frontend

--- game-frontend/src/App.js
+++ game-frontend/src/App.js
@@ -1,25 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './BookList.css';
+import LoginForm from './LoginForm'
 
-function App() {
+function BooksComponent() {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [id, setId] = useState('');
+  const [year, setYear] = useState('');
+  // user is the currently logged in user
+	const [user, setUser] = useState(null);
+  
+  // function to call API to get all books in DB
+  function displayAllBooks() {
+  	axios.get('https://bookstore-404519.ue.r.appspot.com/findAllBooks')
+      .then(response => {
+        setBooks(response.data);  // Axios packs the response in a 'data' property
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error.message);
+        setLoading(false);
+      });
+  
+  };
+
+    // function to call API to get all books in DB
+    function findByAuthor() {
+      axios.get(`https://bookstore-404519.ue.r.appspot.com/findByAuthor?author=${author}`)
+        .then(response => {
+          setBooks(response.data);  // Axios packs the response in a 'data' property
+          setLoading(false);
+        })
+        .catch(error => {
+          setError(error.message);
+          setLoading(false);
+        });
+    
+    };
+
+  // function to handle the user submit of a new book
+  // async used so we can use the "await", which causes a block until post is done
+  //   and makes for a little simpler code (no .then)
+  async function handleSubmit(event) {
+        event.preventDefault();
+        
+        const postData = {
+            title,
+            author,
+            year: parseInt(year, 10) // Convert string to integer
+        };
+
+        try {
+            const response = await axios.post('https://bookstore-404519.ue.r.appspot.com/saveBook', postData);
+            console.log('Response:', response.data);
+            displayAllBooks()
+        } catch (error) {
+            console.error('Error posting data:', error);
+        }
+    };
+
+  
+  // useEffect makes it so list of books shown when this component mounts
+  useEffect(() => {
+    // Using Axios to fetch data
+   
+    displayAllBooks()
+  }, []);
+
+  // this will be called by the LoginForm
+  function HandleLogin(user) {
+    setUser(user);
+  }
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <div className="login-container">
+          <LoginForm LoginEvent={HandleLogin} />
+        </div>
+        <div>
+          {user?
+          <div className="book-list">
+            {books.map(book => (
+              <div className="book-item" key={book.id}>
+                <h3>{book.title}</h3>
+                <p>by {book.author}</p>
+              </div>
+            ))}
+                    
+            <form onSubmit={handleSubmit}>
+              <label>
+                Title:
+                <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
+              </label>
+              <br />
+              <label>
+                Author:
+                <input type="text" value={author} onChange={e => setAuthor(e.target.value)} />
+              </label>
+              <br />
+              <label>
+                Year:
+                <input type="number" value={year} onChange={e => setYear(e.target.value)} />
+              </label>
+              <br />
+              <button type="submit">Submit</button>
+            </form>
+        
+            {/* Form for finding books by author */}
+            <form onSubmit={(e) => {
+              e.preventDefault();
+              findByAuthor(); // Correctly calling the findByAuthor function
+            }}>
+              <label>
+                Find by Author:
+                <input type="text" value={author} onChange={e => setAuthor(e.target.value)} />
+              </label>
+              <button type="submit">Find</button>
+            </form>
+          </div>:<br/>
+          }
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default BooksComponent;
---
+++ game-frontend/src/LoginForm.js
@@ -0,0 +1,76 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, GoogleAuthProvider } from 'firebase/auth';
+import React, {  useState, useEffect } from 'react';
+
+// LoginSuccessful is a function sent in by parent component
+function LoginForm({LoginEvent}) {
+    const firebaseConfig = {
+        apiKey: "AIzaSyAq_bnaOcLpo7O4zflkOGDMKuSrPalfM_k",
+        authDomain: "bookstore-login-1eea9.firebaseapp.com",
+        projectId: "bookstore-login-1eea9",
+        storageBucket: "bookstore-login-1eea9.appspot.com",
+        messagingSenderId: "354232646751",
+        appId: "1:354232646751:web:f42cd9b05075f9e942afea",
+        measurementId: "G-81E46GP6HN"
+      };
+
+	initializeApp(firebaseConfig);
+	
+	const [loggedUser, setLoggedUser] = useState('');
+
+	// function to sign in with Google's page
+	const signInWithGoogle = () => {
+  	
+  		const provider = new GoogleAuthProvider();
+  		const auth = getAuth();
+  		signInWithRedirect(auth, provider)
+    	.then((result) => {
+      		// User signed in
+      		console.log(result.user);
+      		setLoggedUser(result.user)
+      	
+    	}).catch((error) => {
+      	// Handle Errors here.
+      		console.error(error);
+    	});
+	};
+	
+	// function to sign out
+	function logoutGoogle () {
+		const auth=getAuth();
+		auth.signOut();
+		setLoggedUser(null)
+	}
+
+	// we put the onAuthStateChanged in useEffect so this is only called when 
+	// this component mounts  
+	useEffect(() => {
+		const auth = getAuth();
+		auth.onAuthStateChanged(user => {
+			if (user) {
+    			// User is signed in.
+    			console.log("User is signed in:", user);
+    			
+    			
+    			setLoggedUser(user);
+    		
+  			} else {
+    		// No user is signed in.
+    			console.log("No user is signed in.");
+  			}
+  			LoginEvent(user);
+  		});
+	}, []);
+	// note the ? to show either login or logout button
+	return (
+    <div >
+    { loggedUser?
+      <><p>user: {loggedUser.uid}</p> <button onClick={logoutGoogle}>Log out</button> </>
+      :<button onClick={signInWithGoogle}>Sign in with Google</button>
+    } 
+     
+    </div>
+  );
+
+}
+export default LoginForm;
