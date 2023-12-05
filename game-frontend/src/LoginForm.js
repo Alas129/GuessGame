204d0f9e9eed930aeb552a40dcf1aba269229784
Alas129 <alas129@163.com>
2023-12-04 18:24:23 -0800

    Updated LoginForm and Ranking

--- game-frontend/src/LoginForm.js
+++ game-frontend/src/LoginForm.js
@@ -1,76 +1,70 @@
+import React, { useState, useEffect } from 'react';
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithRedirect, GoogleAuthProvider } from 'firebase/auth';
-import React, {  useState, useEffect } from 'react';
 
-// LoginSuccessful is a function sent in by parent component
-function LoginForm({LoginEvent}) {
-    const firebaseConfig = {
-        apiKey: "AIzaSyAq_bnaOcLpo7O4zflkOGDMKuSrPalfM_k",
-        authDomain: "bookstore-login-1eea9.firebaseapp.com",
-        projectId: "bookstore-login-1eea9",
-        storageBucket: "bookstore-login-1eea9.appspot.com",
-        messagingSenderId: "354232646751",
-        appId: "1:354232646751:web:f42cd9b05075f9e942afea",
-        measurementId: "G-81E46GP6HN"
-      };
+function LoginForm({ LoginEvent }) {
+  const firebaseConfig = {
+	apiKey: "AIzaSyAq_bnaOcLpo7O4zflkOGDMKuSrPalfM_k",
+	authDomain: "bookstore-login-1eea9.firebaseapp.com",
+	projectId: "bookstore-login-1eea9",
+	storageBucket: "bookstore-login-1eea9.appspot.com",
+	messagingSenderId: "354232646751",
+	appId: "1:354232646751:web:f42cd9b05075f9e942afea",
+	measurementId: "G-81E46GP6HN"
+  };
 
-	initializeApp(firebaseConfig);
-	
-	const [loggedUser, setLoggedUser] = useState('');
+  initializeApp(firebaseConfig);
 
-	// function to sign in with Google's page
-	const signInWithGoogle = () => {
-  	
-  		const provider = new GoogleAuthProvider();
-  		const auth = getAuth();
-  		signInWithRedirect(auth, provider)
-    	.then((result) => {
-      		// User signed in
-      		console.log(result.user);
-      		setLoggedUser(result.user)
-      	
-    	}).catch((error) => {
-      	// Handle Errors here.
-      		console.error(error);
-    	});
-	};
-	
-	// function to sign out
-	function logoutGoogle () {
-		const auth=getAuth();
-		auth.signOut();
-		setLoggedUser(null)
-	}
+  const [loggedUser, setLoggedUser] = useState('');
 
-	// we put the onAuthStateChanged in useEffect so this is only called when 
-	// this component mounts  
-	useEffect(() => {
-		const auth = getAuth();
-		auth.onAuthStateChanged(user => {
-			if (user) {
-    			// User is signed in.
-    			console.log("User is signed in:", user);
-    			
-    			
-    			setLoggedUser(user);
-    		
-  			} else {
-    		// No user is signed in.
-    			console.log("No user is signed in.");
-  			}
-  			LoginEvent(user);
-  		});
-	}, []);
-	// note the ? to show either login or logout button
-	return (
-    <div >
-    { loggedUser?
-      <><p>user: {loggedUser.uid}</p> <button onClick={logoutGoogle}>Log out</button> </>
-      :<button onClick={signInWithGoogle}>Sign in with Google</button>
-    } 
-     
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+
+    signInWithRedirect(auth, provider)
+      .then((result) => {
+        console.log(result.user);
+        setLoggedUser(result.user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const logoutGoogle = () => {
+    const auth = getAuth();
+    auth.signOut();
+    setLoggedUser(null);
+  };
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log("User is signed in:", user);
+        setLoggedUser(user);
+      } else {
+        console.log("No user is signed in.");
+      }
+      LoginEvent(user);
+    });
+
+    // Cleanup the subscription when the component unmounts
+    return () => unsubscribe();
+  }, [LoginEvent]);
+
+  return (
+    <div>
+      {loggedUser ? (
+        <>
+          <p>User: {loggedUser.uid}</p>
+          <button onClick={logoutGoogle}>Log out</button>
+        </>
+      ) : (
+        <button onClick={signInWithGoogle}>Sign in with Google</button>
+      )}
     </div>
   );
-
 }
+
 export default LoginForm;
--- game-frontend/src/Ranking.js
+++ game-frontend/src/Ranking.js
@@ -1,61 +1,59 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-import { getAuth, signInWithRedirect, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import GameId from './GameId';
 
-function Ranking({gameId}) {
-  console.log(gameId);
-    const [games, setGames] = useState([]);
-    const [usergames, setUserGames] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const auth = getAuth();
+function Ranking({ gameId }) {
+  const [games, setGames] = useState([]);
+  const [userGames, setUserGames] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const auth = getAuth();
 
-    function displayAllGames() {
-        axios.get('https://guessgame-backend.uw.r.appspot.com/findAllGames')
-        .then(response => {
-          setGames(response.data);  // Axios packs the response in a 'data' property
-          setLoading(false);
-        })
-        .catch(error => {
-          setError(error.message);
-          setLoading(false);
-        });
-    
-    };
-    function displayGamesByUserId() {
-        axios.get(`https://guessgame-backend.uw.r.appspot.com/findGameByUserId?userId=${auth.currentUser.email}`)
-        .then(response => {
-            console.log('Response:', response.data);
-            setUserGames(response.data);  // Axios packs the response in a 'data' property
-            setLoading(false);
-          })
-          .catch(error => {
-            setError(error.message);
-            setLoading(false);
-          });
-      };
+  const displayAllGames = () => {
+    axios.get('https://guessgame-backend.uw.r.appspot.com/findAllGames')
+      .then(response => {
+        setGames(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error.message);
+        setLoading(false);
+      });
+  };
 
-      function handlePlayGame(){
-        window.location.href = '/';
-      }
-      function handleDelete(id){
-        axios.get(`https://guessgame-backend.uw.r.appspot.com/deleteGameById?id=${id}`)
-        .then(response => {
-          window.location.reload();
-        })
-        .catch(error => {
-          setError(error.message);
-          setLoading(false);
-        });
-      }
+  const displayGamesByUserId = () => {
+    axios.get(`https://guessgame-backend.uw.r.appspot.com/findGameByUserId?userId=${auth.currentUser.email}`)
+      .then(response => {
+        setUserGames(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error.message);
+        setLoading(false);
+      });
+  };
 
-    useEffect(() => {
-        // Using Axios to fetch data
-        displayAllGames()
-        displayGamesByUserId()
-      }, []);
+  const handlePlayGame = () => {
+    window.location.href = '/';
+  };
+
+  const handleDelete = (id) => {
+    axios.get(`https://guessgame-backend.uw.r.appspot.com/deleteGameById?id=${id}`)
+      .then(response => {
+        window.location.reload();
+      })
+      .catch(error => {
+        setError(error.message);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    displayAllGames();
+    displayGamesByUserId();
+  }, []);
+
   return (
     <div className="game-list">
       <div className="all-game-list">
@@ -64,27 +62,27 @@ function Ranking({gameId}) {
           <h1>Top 10 game records</h1>
         </div>
         {games.map(game => (
-          <div className="game-item">
-            <p>{game.userId} score: {game.score} at {game.date}</p> 
+          <div className="game-item" key={game.id}>
+            <p>{game.userId} score: {game.score} at {game.date}</p>
           </div>
         ))}
       </div>
       <div className="user-game-list">
-      <div className="game-list-header">
-        <h1>{auth.currentUser.email}</h1>
-        <h1>All game records</h1>
+        <div className="game-list-header">
+          <h1>{auth.currentUser.email}</h1>
+          <h1>All game records</h1>
         </div>
-        {usergames.map(usergame => (
-          <div className="game-item">
-            {usergame.userId} score: {usergame.score} at {usergame.date}<button className = "delete-button"onClick={()=>handleDelete(usergame.id)}>Delete</button>
+        {userGames.map(userGame => (
+          <div className="game-item" key={userGame.id}>
+            <p>{userGame.userId} score: {userGame.score} at {userGame.date}</p>
+            <button className="delete-button" onClick={() => handleDelete(userGame.id)}>Delete</button>
           </div>
         ))}
       </div>
-      <div >
-        <button className="play-game-button" onClick={handlePlayGame}>Play Game</button> 
+      <div>
+        <button className="play-game-button" onClick={handlePlayGame}>Play Game</button>
         <p className="show-game-id">currentGameId: {gameId}</p>
       </div>
-     
     </div>
   );
 }
